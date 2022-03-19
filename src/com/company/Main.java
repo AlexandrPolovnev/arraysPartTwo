43502577d6e203339a48c73fa2f05c71e1c4169c
AlexandrPolovnyov <polovniasha92@gmail.com>
2022-03-19 23:02:10 +0300

    Я не смог разобраться с остальными, нужно больше времени (

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
-public class Main {
+import javax.sql.rowset.spi.SyncFactory;
+import java.awt.*;
+
+public class Main<arr> {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr = generateRandomArray();
     }
-}
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        int minArr = 0;
+        int maxArr = 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+            sum = sum + arr[i];
+            if (i == arr.length - 1) {
+                System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+            }
+        }
+            return arr;
+        }
+    }
+
+
+
